01c4c66b52e0dc1427895c0997c0310a22144735
8845musign <8845musign@gmail.com>
2016-01-15 11:46:33 +0900

    Add comment

--- client/js/containers/App.jsx
+++ client/js/containers/App.jsx
@@ -1,17 +1,25 @@
+// Libaray
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+// Module
 import * as actionChat from '../actions/Chat'
-
+// Views
 import ChatForm from '../components/ChatForm.jsx'
 import ListChat from '../components/ListChat.jsx'
 import ListParticipant from '../components/ListParticipant.jsx'
 import ParticipantSelf from '../components/PaticipantSelf.jsx'
 
-
+/**
+ * Container View
+ * ReduxとReactの接続を行う
+ * ReduxとReactが関連づくのはこのレイヤーのみ
+ * 子としてぶら下がるViewは渡って来たpropのstateやmethodを利用するだけ
+ */
 class App extends Component {
   render() {
+    // dispatchメソッド
     let { onSentMessage } = this.props;
+    // Storeから渡されたStateを分割して読み込み
     let { self, messages, members } = this.props;
     
     return (
@@ -37,20 +45,29 @@ class App extends Component {
   }
 }
 
+/**
+ * Storeから渡されたstateをmappingしてContainerへ渡す
+ */
 function mapStateToProps(state) {
   return {
-    self: state.chat.self,
-    messages: state.chat.messages,
-    members: state.chat.members
+    self: state.chatReducer.self,
+    messages: state.chatReducer.messages,
+    members: state.chatReducer.members
   }
 }
 
+/**
+ * StoreのdispatchとActionCreatoreを合成、mappingしてContainerへ渡す
+ */
 function mapDispatchToProps(dispatch) {
   return {
     onSentMessage: (message) => dispatch(actionChat.sentMessage(message))
   }
 }
 
+/**
+ * react-reduxのconnectにより、reduxとreact(Container)を接続
+ */
 export default connect(
   mapStateToProps,
   mapDispatchToProps
