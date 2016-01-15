2d53885bc57d07ec360ec6f1199398c70b98eb92
8845musign <8845musign@gmail.com>
2016-01-15 11:15:47 +0900

    Add comment

--- client/js/index.js
+++ client/js/index.js
@@ -1,28 +1,38 @@
+// Library - react
 import React from 'react'
 import ReactDom from 'react-dom'
+// Library - react-redux
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+// Library - socket.io
 import io from 'socket.io-client'
 
+// Module
 import socketMiddleware from './middlewares/socketMiddleware'
 import rootReducer from './reducers'
 import * as actionTypes from './constants/ActionTypes'
 import * as actionChat from './actions/Chat'
 import App from './containers/App.jsx'
 
+// 入室時の名前の入力
 let name = prompt("名前を入力してください");
 
+// サーバーとのコネクト
 const socket = io("http://localhost:3000/", { query: 'name=' + name });
 
+// StoreオブジェクトへSocket.io処理を挟む
+// composeはMiddlewareとStoreを合成してコンストラクタを返す
 const createStoreWithMiddleware = compose(
   applyMiddleware(socketMiddleware(socket))
 )(createStore);
 
 const store = createStoreWithMiddleware(rootReducer, {});
 
+// サーバーからの通信の受け取り
 socket.on(actionTypes.ENTER_MEMBER, data => store.dispatch(actionChat.enterMember(data)));
 socket.on(actionTypes.RECEIVE_MESSAGE, data => store.dispatch(actionChat.receiveMessage(data)));
 
+// selfの入室
 socket.on('connect', function(){
   var self = {
     id: socket.id,
@@ -31,6 +41,7 @@ socket.on('connect', function(){
   store.dispatch(actionChat.enter(self));
 });
 
+// reactによる画面の描写
 ReactDom.render(
   <Provider store={store}>
     <App />
