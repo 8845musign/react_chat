c00abd9fc179fb9c26f3620473a35edf71a91419
8845musign <8845musign@gmail.com>
2016-01-15 11:40:17 +0900

    Add comment

--- client/js/middlewares/socketMiddleware.js
+++ client/js/middlewares/socketMiddleware.js
@@ -1,8 +1,12 @@
 import * as actionTypes from '../constants/ActionTypes'
 
+/**
+ * サーバーへの通信をactionCreatorとreducerの間に挟む
+ */
 export default socket => store => next => action => {
+  // meta.remoteがtrueの物はserverへもイベントをemit
   if (action.meta && action.meta.remote) {
-    action.id = socket.id;
+    action.id = socket.id; // IDを付与
     socket.emit(action.type, action);
   }
   
